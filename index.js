470a8540f875cc32f16c3d77d3b045f1c75814e3
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-07-12 18:50:48 +0100

    master

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 const myEmojis = ["😂","👋🏻","💪"]
 const emojiContainer = document.getElementById("emoji-container")
 
+function renderEmo(){
 for (let i = 0 ; i < myEmojis.length ; i++){
     const emoji = document.createElement('span')
     emoji.textContent = myEmojis[i]
     emojiContainer.append(emoji)
 
-}
+}}
 
-
+renderEmo()
 const pushBtn = document.getElementById("push-btn")
 
 pushBtn.addEventListener("click", function(){
-    
-    
-    renderEmo()
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value){
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        emojiContainer.innerHTML = ""
+        renderEmo()
+    }
 })
-
-function renderEmo(){
-        const emojiInput = document.getElementById("emoji-input")
-        if (emojiInput.value){
-            myEmojis.push(emojiInput.value)
-            emojiInput.value = ""
-        }
-    
-    
-    
-}
