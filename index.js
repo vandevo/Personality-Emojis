17758dd3681c54808fb612b9708fc892b2dcf42e
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-07-12 18:25:48 +0100

    master

---
+++ index.js
@@ -0,0 +1,9 @@
+const myEmojis = ["😂","👋🏻","💪"]
+const emojiContainer = document.getElementById("emoji-container")
+
+for (let i = 0 ; i < myEmojis.length ; i++){
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+
+}
